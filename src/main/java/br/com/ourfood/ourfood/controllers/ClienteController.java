db246582c8698a96ed4ab00fb18aee1c284979d2
larissaslff <110000521+larissaslff@users.noreply.github.com>
2022-12-15 19:57:25 -0300

    Busca produto por id

--- src/main/java/br/com/ourfood/ourfood/controllers/ClienteController.java
+++ src/main/java/br/com/ourfood/ourfood/controllers/ClienteController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/clientes")
@@ -22,4 +23,5 @@ public class ClienteController {
     public List<Cliente> buscarTodos(){
         return repository.findAll();
     }
+
 }
--- src/main/java/br/com/ourfood/ourfood/controllers/ProdutosController.java
+++ src/main/java/br/com/ourfood/ourfood/controllers/ProdutosController.java
@@ -1,11 +1,13 @@
 package br.com.ourfood.ourfood.controllers;
 
+import br.com.ourfood.ourfood.entities.Cliente;
 import br.com.ourfood.ourfood.entities.Produtos;
 import br.com.ourfood.ourfood.repositories.ProdutosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/produtos")
@@ -21,5 +23,10 @@ public class ProdutosController {
     @PostMapping
     public void novoProduto(@RequestBody Produtos produto) {
         repository.save(produto);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Produtos> buscarPorId(@PathVariable Long id){
+        return repository.findById(id);
     }
 }
